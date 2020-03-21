d49ff950aed1fe12749425f0dfa74c1ca09c6605
Bernard Longho <19242877+blongho@users.noreply.github.com>
2020-03-21 20:47:35 +0100

    Adds circle-ci config file

--- sample/src/androidTest/java/com/blongho/sample/ExampleInstrumentedTest.java
+++ sample/src/androidTest/java/com/blongho/sample/ExampleInstrumentedTest.java
@@ -3,17 +3,12 @@ package com.blongho.sample;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
+@RunWith(JUnit4.class)
 public class ExampleInstrumentedTest {
 
   @Test
